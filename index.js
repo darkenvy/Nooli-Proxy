7b2115869966d5569755ef32ba7c2575c0472944
darkenvy <darkenvy6@gmail.com>
2017-02-15 12:55:48 -0800

    cleaned up old files

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
-var http      = require('http'),
-    httpProxy = require('http-proxy'),
-    harmon    = require('harmon'),
-    express   = require('express'),
-    app       = express();
+var express = require('express'),
+    cheerio = require('cheerio'),
+    request = require('request'),
+    app = express();
 
 
-var acceptMimes = [
-  /html/,
-  /xhtml/
-  // 'text/html',
-  // 'application/xhtml+xml'
-]
 
 
-var proxy = httpProxy.createProxyServer({});
-
-// app.use(harmon([], selects));
-app.get(/.*/, function(req, res) { 
-  // proxy.web(req, res, { target: 'http://www.fatfingers.com/default.aspx' }, function(e) {
-  //   res.send(e);
-  // });
-  if (req.headers.accept.match(/xhtml/) ||
-      req.headers.accept.match(/html/)) {
-    console.log('1', req.headers.host);
-    // console.log(req.url, req.headers.accept);
-    proxy.web(req, res, { target: req.headers.host });
-  } else {
-    console.log('ELSE');
-    res.send('');
-  }
-
-  // proxy.on('error', function (err, req, res) {
-  //   res.writeHead(500, {'Content-Type': 'text/plain'});
-  //   console.log('ERROR: ', err);
-  //   res.end('Something went wrong. And we are reporting a custom error message.');
-  // });
-
-  // console.log(req.headers.host);
-  // proxy.web(req, res, { target: 'http://www.fatfingers.com/rw_common/themes/fatfingers/css/colour_scheme/day4.css' });
-  // proxy.web(req, res, { target: 'http://www.fatfingers.com/default.aspx' });
-  // proxy.web(req, res, { target: req.headers.host });
+app.get('/', (req,res) => {
+  request('http://www.3dsiso.com/cia-downloads/', (err, response, html) => {
+    // let $ = cheerio.load(html);
+    // console.log($);
+    // console.log(html);
+    res.send(html);
+  })
 })
 
-console.log('Hosting server on port 3000');
+
+
+console.log("Regular Express server listening on port 3000");
 var server = app.listen(process.env.PORT || 3000);
